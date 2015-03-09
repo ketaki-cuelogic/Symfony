998d1574f879c6beeecdd2bbc195000b21ba6840
Ketaki Yeolekar <ketaki.yeolekar@cuelogic.co.in>
2015-03-09 15:43:50 +0530

    hello project

---
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HelloController extends Controller {
+
+	public function indexAction( $name ) {
+		//return new Response('<html><body>Hello '.$name.'!</body></html>');
+		return $this->render( 'AcmeHelloBundle:Hello:index.html.twig',
+						array( 'name' => $name )
+			);
+	}
+
+}
+
+?>
